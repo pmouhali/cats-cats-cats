a7475dd6694125d96f9ff217749ba36700be51e2
HenriBrg <hnr.berger@gmail.com>
2018-11-27 13:49:04 +0100

    Saves V1

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,19 @@
 class CartsController < ApplicationController
 
 
+  def destroy
+    @cart_with_item = Cart.find(current_user.id)
+    if @cart_with_item.items.where(name: @item_to_delete.name).destroy
+      flash[:notice] = "Le chat a été retiré du panier"
+    else
+      flash[:error] = "Erreur"
+    end
 
-  
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
+  end
 
 end
