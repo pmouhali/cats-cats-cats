be0583e34b419f8436994c4a5eae47c20d1582c3
pmouhali <p.m.272019@gmail.com>
2018-11-28 17:16:43 +0100

    retry les deux controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-	helper_method :@current_user
+	helper_method :current_user
 	before_action :sanitize_devise_params, if: :devise_controller?
 
 	def sanitize_devise_params
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 	end
 
-	def @current_user
+	def current_user
 		if session[:user_id]
 			return User.find(session[:user_id])
 		else
