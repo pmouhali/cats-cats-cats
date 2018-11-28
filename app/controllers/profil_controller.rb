0edf900823f2c8a5820c3f5780d09ea0f32baadb
HenriBrg <hnr.berger@gmail.com>
2018-11-28 12:14:37 +0100

    Fix accessibility to other profil page

--- app/controllers/profil_controller.rb
+++ app/controllers/profil_controller.rb
@@ -1,7 +1,7 @@
 class ProfilController < ApplicationController
   def show
 
-    if !current_user.nil?
+    if !current_user.nil? 
       @user = User.find(params[:id])
     else
       flash[:danger] = "You must be logged to reach this page"
