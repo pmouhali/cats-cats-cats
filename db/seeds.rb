6f33e6686dd68f8b1c693f0eb8c05ba71c3e3481
vrattanasamay <victor.rattanasamay@gmail.com>
2018-11-28 15:50:26 +0100

    all english

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-Category.create(name: "Chat Intelligent",description: "Ces chats savent ouvrir les portes, parfois même lire.")
-Category.create(name: "Chat Calin",description: "Ces chats sont très affectueux, mais ducou ils sont jaloux donc attention aux étrons sous l'oreiller.")
-Category.create(name: "Chat Démoniaque",description: "A vos risques et périls")
+Category.create(name: "Smart cat",description: "These cats can open doors, sometimes even read.")
+Category.create(name: "Cuddly cat",description: "These cats are very affectionate, but as a result they are jealous so watch out for turds under the pillow..")
+Category.create(name: "Demonic cat",description: "At your own risk and peril.")
 
 j = 1
 6.times do
@@ -8,7 +8,7 @@ i = Item.new
 i.name = Faker::Cat.name
 i.price = Faker::Number.number(3)
 i.image = "cat" + j.to_s + ".jpg"
-i.description = "Je suis un chat, donc je miaule tout le temps, et je chie sur tes draps quand je suis pas content."
+i.description = "I'm a cat, so I meow all the time, and I shit on your sheets when I'm not happy."
 i.category = Category.find(Faker::Number.between(1,3))
 i.save
 j += 1
