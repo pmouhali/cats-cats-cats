edecfc469adf8979b808461f705555bd20107bd2
pmouhali <p.m.272019@gmail.com>
2018-11-28 17:06:09 +0100

    retry charges_controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -23,6 +23,13 @@ class ChargesController < ApplicationController
 	  flash[:error] = e.message
 	  redirect_to new_charge_path
 	end
+
+	if session[:user_id]
+		current_user = User.find(session[:user_id])
+	else
+		current_user = nil
+	end
+
 	if !current_user.nil?
 		@user_who_order = current_user.email
 	end
